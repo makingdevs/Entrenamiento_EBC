5fa295cd7b64fd22ea135e827d3155482bc18ec9
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-04 10:16:42 -0600

    Paso 6: POGO's y quito paréntesis

--- HolaMundo.groovy
+++ HolaMundo.groovy
@@ -5,8 +5,14 @@ class HolaMundo {
   def saluda() { "Hola ${nombre} !!!" }
 
   static main(args) {
-    def obj = new HolaMundo()
-    obj.setNombre("Juan - Paso 5")
-    println(obj.saluda())
+    def obj = new HolaMundo(nombre: "EBC")
+    println obj.saluda()
+    obj.setNombre("Juan - Paso 6")
+    println obj.saluda()
+    obj.nombre = "MakingDevs"
+    println obj.saluda()
+    obj["nombre"] = "Hola Inmundo"
+    println obj.saluda()
+    println obj.nombre
   }
 }
