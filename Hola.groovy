46af7862f645df28f213636399351fb1db0268c5
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-04 11:10:31 -0600

    Transformo de Java A Groovy para obtener paquetes por default

--- Hola.groovy
+++ Hola.groovy
@@ -1,15 +1,9 @@
-import java.util.*;
-import java.net.*;
-
 public class Hola {
 
   public static void main(String[] args){
     Date d = new Date();
     System.out.println(d);
-    try{
     URL url = new URL("https://makingdevs.com");
     System.out.println(url);
-    } catch(Exception e) {
-    }
   }
 }
