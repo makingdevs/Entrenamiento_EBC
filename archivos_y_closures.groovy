7f1bf12613232200a956863fa93acc3a0ef45669
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-07 12:38:12 -0600

    Uso Enum para definir el Sexo

--- archivos_y_closures.groovy
+++ archivos_y_closures.groovy
@@ -10,16 +10,33 @@ class Person {
   String name
   String lastName
   String email
-  String genre
+  Genre genre
   String ipAddress
+}
+
+enum Genre {
+  Male,
+  Female,
+  Bigender,
+  Genderqueer,
+  Agender,
+  Genderfluid,
+  Non_binary,
+  Polygender
 }
 
 people = []
 // Quito el encabezado
 lineas.drop(1).each { linea ->
   attrs = linea.split(",")
-  person = new Person(id: attrs[0].toInteger(), name: attrs[1], lastName: attrs[2], email:attrs[3], genre: attrs[4], ipAddress: attrs[5])
+  person = new Person(
+    id: attrs[0].toInteger(),
+    name: attrs[1],
+    lastName: attrs[2],
+    email:attrs[3],
+    genre: Genre.valueOf(attrs[4].replaceAll("-", "_")),
+    ipAddress: attrs[5])
   people << person
 }
 
-println people
+println people.findAll { p -> p.genre != "Male" && p.genre != "Female" }
