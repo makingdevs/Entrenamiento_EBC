04574b2e50b326fc0d456315afd6b9c7e7ef7b4e
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-07 11:17:23 -0600

    Agrego solucion de AoC con listas y closures

--- aoc_2015_d1_p1.groovy
+++ aoc_2015_d1_p1.groovy
@@ -10,3 +10,20 @@ for(int i = 0; i < s.size(); i++) {
 }
 
 println contador
+
+// ---------------------------------
+
+numeros = []
+s.split("").each {
+  if(it == "(") numeros.add(1)
+  else numeros.add(-1)
+}
+numeros.sum()
+
+// ---------------------------------
+
+s.split("").collect { if(it == "(") 1 else -1 }.sum()
+
+// ---------------------------------
+
+s.split("").collect { it == "(" ? 1 : -1 }.sum()
