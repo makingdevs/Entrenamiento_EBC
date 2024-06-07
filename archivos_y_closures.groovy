27912a1a22d45a64ba437eaee6824a537f037c83
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-07 12:15:25 -0600

    Leo el archivo y parto las líneas

---
+++ archivos_y_closures.groovy
@@ -0,0 +1,24 @@
+texto = new File("./MOCK_DATA.csv").text
+
+lineas = texto.split("\n")
+
+println lineas.size()
+
+class Person {
+  Integer id
+  String name
+  String lastName
+  String email
+  String genre
+  String ipAddress
+}
+
+people = []
+
+lineas.each { linea ->
+  attrs = linea.split(",")
+  person = new Person(id: attrs[0], name: attrs[1], lastName: attrs[2], email:attrs[3], genre: attrs[4], ipAddress: attrs[5])
+  people << person
+}
+
+println people
