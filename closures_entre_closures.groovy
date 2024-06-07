f302829596460a02b7908b5d746dc34812ec0a25
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-07 10:47:06 -0600

    Closure entre closures

---
+++ closures_entre_closures.groovy
@@ -0,0 +1,13 @@
+// area = {base, altura -> base * altura / 2}
+
+def area_triangulo(base, altura) {
+    area_rectangulo(base, altura) / 2
+}
+
+def area_rectangulo(base, altura) {
+    base * altura
+}
+
+println "Rectángulo(3,4) = Area ${area_rectangulo(3, 4)}"
+
+println "Triángulo(3,4) = Area ${area_triangulo(3, 4)}"
