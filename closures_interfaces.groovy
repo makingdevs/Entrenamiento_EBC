2424d6b59cca40779365c11ddc3b680c28d402de
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-11 11:43:41 -0600

    Agrego un mapa como closure a la interfaz

--- closures_interfaces.groovy
+++ closures_interfaces.groovy
@@ -18,4 +18,29 @@ displayMouseLocation = { positionLabel.text = "${it.x}, ${it.y}" }
 frame.addMouseListener( displayMouseLocation as MouseListener )
 frame.addMouseMotionListener( displayMouseLocation as MouseMotionListener )
 
+// Un mapa de closures para una interfaz con varios métodos
+msgLabel = new JLabel("")
+frame.contentPane.add(msgLabel)
+frame.contentPane.add(new JTextField())
+// handleFocus = [
+//   focusGained: {msgLabel.text = "Gained"},
+//   focusLost: {msgLabel.text = "Lost"}
+// ]
+
+//button.addFocusListener(handleFocus as FocusListener)
+
+handleFocus = [
+    focusGained: { FocusEvent e ->
+      println "Gained Closure"
+      msgLabel.text = "Gained Closure"
+    },
+
+    focusLost: { FocusEvent e ->
+      println "Lost Closure"
+      msgLabel.text = "Lost Closure"
+    }
+  ]
+
+button.addFocusListener(handleFocus  as FocusListener)
+
 frame.show()
