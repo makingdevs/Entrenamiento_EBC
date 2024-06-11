26e70d2fde1d3c7eec84460995de846da0f6916b
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-11 11:52:32 -0600

    Tipado dinámico en Groovy

---
+++ tipados.groovy
@@ -0,0 +1,22 @@
+// def saluda(s) {
+//   println s.toUpperCase()
+// }
+//
+// saluda("hola")
+// saluda('c')
+// saluda(3828)
+
+//class P {
+//  String nombre
+//  String apellido
+//  String getFullName() { "$nambre $apellido" }
+//}
+//
+//def p = new P(nombre: "Juan", apellido: "Reyes")
+
+//int metodo() {
+//  if(false) { 'String' }
+//  else { 42 }
+//}
+//
+//println metodo()
