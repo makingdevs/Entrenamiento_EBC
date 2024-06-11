5d1393a8653133285a7c717e71eb7ca3a35225d5
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-11 11:22:59 -0600

    Impl varias interfaces y métodos con el mismo closure

--- closures_interfaces.groovy
+++ closures_interfaces.groovy
@@ -4,13 +4,18 @@ import java.awt.event.*
 
 frame = new JFrame(size: [300,300], layout: new FlowLayout(), defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE)
 
+// Sección de un sólo boton, para una interfaz con un solo método
 button = new JButton("Click")
 frame.getContentPane().add(button)
-
-
-
 button.addActionListener({ event ->
   println "Presionaste el botón de nuevo con Groovy"
 } as ActionListener)
+
+// Uso un Closure para varios métodos en varias interfaces
+positionLabel = new JLabel("")
+frame.contentPane.add(positionLabel)
+displayMouseLocation = { positionLabel.text = "${it.x}, ${it.y}" }
+frame.addMouseListener( displayMouseLocation as MouseListener )
+frame.addMouseMotionListener( displayMouseLocation as MouseMotionListener )
 
 frame.show()
