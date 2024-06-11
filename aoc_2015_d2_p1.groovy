8a88a0c2984e91b612cfe8c295d4a2b17508ac45
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-11 09:46:08 -0600

    Agrego solución de AoC básica para D2P1

---
+++ aoc_2015_d2_p1.groovy
@@ -0,0 +1,32 @@
+area_alto = { l, w, h -> 2 * l * w}
+area_ancho = { l, w, h -> 2 * w * h}
+area_largo = { l, w, h -> 2 * h * l}
+
+area_lado_pequeño = {l, w, h ->
+  [
+    l*w,
+    l*h,
+    w*h
+  ].min()
+}
+
+def papelNecesarioParaLaCaja(caja) {
+  lados = caja.split("x")
+  alto = lados[0].toInteger()
+  ancho = lados[1].toInteger()
+  largo = lados[2].toInteger()
+
+  area_alto(alto, ancho, largo) +
+  area_ancho(alto, ancho, largo) +
+  area_largo(alto, ancho, largo) +
+  area_lado_pequeño(alto, ancho, largo)
+}
+
+regalo1 = "2x3x4"
+regalo2 = "1x1x10"
+
+papel_necesario_1 = papelNecesarioParaLaCaja(regalo1)
+papel_necesario_2 = papelNecesarioParaLaCaja(regalo2)
+
+println papel_necesario_1
+println papel_necesario_2
