f1a5ed00bb14d14b61070063e2be67c8c95324cc
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-11 10:56:52 -0600

    Instancio la clase directo en el método

--- closures_interfaces.groovy
+++ closures_interfaces.groovy
@@ -10,11 +10,10 @@ frame.getContentPane().add(button)
 
 class MyActionListener implements ActionListener {
   void actionPerformed(ActionEvent event){
-    println "Presionaste el botón"
+    println "Presionaste el botón de nuevo"
   }
 }
-l = new MyActionListener()
 
-button.addActionListener(l)
+button.addActionListener(new MyActionListener())
 
 frame.show()
