310ae4cdc0c13a8ea95aba0967e9cade8a1444dd
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-11 10:46:50 -0600

    Agrego un frame simple

---
+++ closures_interfaces.groovy
@@ -0,0 +1,7 @@
+import javax.swing.*
+import java.awt.*
+import java.awt.event.*
+
+frame = new JFrame(size: [300,300], layout: new FlowLayout(), defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE)
+
+frame.show()
