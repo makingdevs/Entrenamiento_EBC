3ea454904e026f0d4d7a9818e37a94cd47d009ea
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-13 12:05:35 -0600

    Llamada a un SP de Oracle con OracleTypes

--- my_sql.groovy
+++ my_sql.groovy
@@ -4,6 +4,8 @@
 )
 
 import groovy.sql.Sql
+import oracle.jdbc.OracleTypes
+import groovy.sql.OutParameter;
 
 def db = [
   url:'jdbc:oracle:thin:@172.16.60.17:3521:EBCDEVL',
@@ -18,4 +20,21 @@ query = "select * from FALTASRMG where NIDFALTAS <= 109"
 // }
 lista = sql.rows(query)
 println lista.size()
- println lista*.NIDFALTAS
+println lista*.NIDFALTAS
+
+
+CURSOR_PARAMETER = new OutParameter() {
+  public int getType() {
+    return OracleTypes.CURSOR;
+  }
+};
+
+P_CODE = '202420'
+sp = "{call GENPUB.PC_APIBANNER_ALL_TERMS.PR_ALL_TERMS(?, ?)}"
+sql.callWithAllRows(sp, [P_CODE, CURSOR_PARAMETER]) { cursor ->
+  cursor.eachRow { row ->
+    def columna1 = row.TERM_CODE
+    def columna2 = row.TERM_DESC
+    println "Columna 1: $columna1, Columna 2: $columna2"
+  }
+}
