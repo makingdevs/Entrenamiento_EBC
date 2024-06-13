f5fa24ef28b11fcf9fc799dd04e50cb7e9ab5e00
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-13 10:17:28 -0600

    Leo más nodos del XMl con NS

--- lector_xml_ns.groovy
+++ lector_xml_ns.groovy
@@ -1,6 +1,6 @@
 import groovy.xml.XmlSlurper
 
-factura = new XmlSlurper().parse("Factura_13475.xml").declareNamespace(cfdi: "cfdi")
+factura = new XmlSlurper().parse("Factura_13475.xml").declareNamespace(cfdi: "http://www.sat.gob.mx/cfd/4")
 println "${factura.@Fecha} - ${factura.@Folio} - ${factura.@Moneda}"
 
 println "*"*50
@@ -8,3 +8,19 @@ println "*"*50
 factura.Conceptos.Concepto.each {
   println it.@Descripcion
 }
+
+println "*"*50
+factura.Conceptos.'cfdi:Concepto'.each { c ->
+  c.Impuestos.'cfdi:Traslados'.Traslado.each { t ->
+    println t.@Base
+  }
+}
+
+println "*"*50
+
+factura.'cfdi:Traslados'.Traslado.each { t ->
+  println t.@Base
+}
+
+println factura.Conceptos.Concepto*.'@ClaveUnidad'
+println factura.'cfdi:Concepto'*.'@ClaveUnidad'
