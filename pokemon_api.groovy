de05225dcead95dc0f40235da274c6be2aad02a4
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-14 11:56:43 -0600

    Uso simple de la API de pokemon

---
+++ pokemon_api.groovy
@@ -0,0 +1,14 @@
+import groovy.json.JsonSlurper
+
+url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+
+httpConnection = new URL(url)
+
+jsonSlurper = new JsonSlurper()
+json = jsonSlurper.parseText(httpConnection.text)
+
+json.sprites.findAll{ k, v -> v.class == String }.each { k, v ->
+
+  println v
+
+}
