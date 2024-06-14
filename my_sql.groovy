8f443e7f49d76589d912c48b9a258f3b26ff253c
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-14 10:02:37 -0600

    Aplico transacciones con Groovy

--- my_sql.groovy
+++ my_sql.groovy
@@ -44,35 +44,45 @@ sql.callWithAllRows(sp, [P_CODE, CURSOR_PARAMETER]) { cursor ->
   }
 }
 
-id = new Date().time.toString()
-nombre = "Juan"
-paterno = "Reyes"
-materno = "Zuñiga"
+counter = sql.firstRow('SELECT COUNT(*) as num FROM EMPLEADOSPRUEBA').num
+println counter
 
-insert_query = """
-  insert into EMPLEADOSPRUEBA(TRAB_ID, NOMBRE, PATERNO, MATERNO)
-  values(?, ?, ?, ?)
-"""
-sql.execute insert_query, [id, nombre, paterno, materno]
+sql.withTransaction {
+  id = new Date().time.toString()
+  nombre = "Juan"
+  paterno = "Reyes"
+  materno = "Zuñiga"
 
-insert_query = """
-  insert into EMPLEADOSPRUEBA(TRAB_ID, NOMBRE, PATERNO, MATERNO)
-  values(:id, :nombre, :paterno, :materno)
-"""
-sql.execute insert_query, [paterno: "Barbachano", materno: "EBC", nombre: "Gerardo", id: new Date().time]
+  insert_query = """
+    insert into EMPLEADOSPRUEBA(TRAB_ID, NOMBRE, PATERNO, MATERNO)
+    values(?, ?, ?, ?)
+  """
+  sql.execute insert_query, [id, nombre, paterno, materno]
 
-results = sql.execute "update EMPLEADOSPRUEBA set PATERNO=? where PATERNO = ?", ["NUEVO", "Barbachano"]
+  insert_query = """
+    insert into EMPLEADOSPRUEBA(TRAB_ID, NOMBRE, PATERNO, MATERNO)
+    values(:id, :nombre, :paterno, :materno)
+  """
+  sql.execute insert_query, [paterno: "Barbachano", materno: "EBC", nombre: "Gerardo", id: new Date().time]
 
-println results
-println sql.updateCount
+  results = sql.execute "update EMPLEADOSPRUEBA set PATERNO=? where PATERNO = ?", ["NUEVO", "Barbachano"]
 
-sql.eachRow("select * from EMPLEADOSPRUEBA"){ empleado ->
-  println empleado
+  println results
+  println sql.updateCount
+
+  sql.eachRow("select * from EMPLEADOSPRUEBA"){ empleado ->
+    println empleado
+  }
+
+  println "-"*50
+  dataset = sql.dataSet("EMPLEADOSPRUEBA")
+  dataset.add([paterno: "Pineda", materno: "EBC", nombre: "Reyna", trab_id: new Date().time])
+
+  busqueda = dataset.findAll { p -> p.paterno == 'Pineda' }
+  busqueda.each { println it }
+
+  throw new RuntimeException("ERROR")
 }
 
-println "-"*50
-dataset = sql.dataSet("EMPLEADOSPRUEBA")
-dataset.add([paterno: "Pineda", materno: "EBC", nombre: "Reyna", trab_id: new Date().time])
-
-busqueda = dataset.findAll { p -> p.paterno == 'Pineda' }
-busqueda.each { println it }
+counter2 = sql.firstRow('SELECT COUNT(*) as num FROM EMPLEADOSPRUEBA').num
+println counter2
