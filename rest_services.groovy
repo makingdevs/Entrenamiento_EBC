aebd0f8e06ca281d4b87a0dae64d1a6647d7a8db
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-14 12:52:22 -0600

    Estructuro todo en una clase Singleton

--- rest_services.groovy
+++ rest_services.groovy
@@ -11,9 +11,18 @@ class AcademicLevel {
   Integer id
 }
 
-def client = new RESTClient("https://api-tramites.ebc.edu.mx/api-tramites-escolares")
-def response = client.get(path:'/v1/api/academic_level', query:[bannerLevel: true])
+@Singleton
+class APITramites {
+  def url = "https://api-tramites.ebc.edu.mx/api-tramites-escolares"
+  def client = new RESTClient(url)
 
-//println response.properties
-println "*"*30
-println response.json.collect { new AcademicLevel(it) }
+  List<AcademicLevel> allAcademicLevels(Boolean bannerLevel) {
+    def response = client.get(
+      path:'/v1/api/academic_level',
+      query:[bannerLevel: bannerLevel])
+    response.json.collect { new AcademicLevel(it) }
+  }
+}
+
+println APITramites.instance.allAcademicLevels(true)
+println APITramites.instance.allAcademicLevels(false)
