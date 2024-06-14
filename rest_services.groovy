401df0a67b21acb8651bc661521c077ceb4f2b84
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-14 12:46:37 -0600

    Mapeo de un objeto a una petición de servicios

--- rest_services.groovy
+++ rest_services.groovy
@@ -4,11 +4,16 @@
 
 import wslite.rest.*
 
+@groovy.transform.Canonical
+class AcademicLevel {
+  String bannerId
+  String description
+  Integer id
+}
+
 def client = new RESTClient("https://api-tramites.ebc.edu.mx/api-tramites-escolares")
 def response = client.get(path:'/v1/api/academic_level', query:[bannerLevel: true])
 
-println response.properties
+//println response.properties
 println "*"*30
-response.json.each {
-  println it
-}
+println response.json.collect { new AcademicLevel(it) }
