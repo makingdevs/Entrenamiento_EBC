7dccb0e7df42000dd156713746e879817a56902f
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-15 08:27:09 -0600

    Agrego un nuevo endpoint

--- rest_services.groovy
+++ rest_services.groovy
@@ -3,12 +3,21 @@
 )
 
 import wslite.rest.*
+import groovy.transform.Canonical
 
-@groovy.transform.Canonical
+@Canonical
 class AcademicLevel {
   String bannerId
   String description
   Integer id
+}
+
+@Canonical
+class EBCPerson {
+  Integer pidm
+  String firstName
+  String lastName
+  String idEnrollment
 }
 
 @Singleton
@@ -22,7 +31,16 @@ class APITramites {
       query:[bannerLevel: bannerLevel])
     response.json.collect { new AcademicLevel(it) }
   }
+
+  EBCPerson findPersonBy(Integer pidm = 0, String username = "", String idEnrollment = ""){
+    def response = client.get(
+      path:'/v1/api/persons',
+      query:[idEnrollment: idEnrollment, pidm: pidm, username: username])
+    new EBCPerson(response.json)
+  }
 }
 
 println APITramites.instance.allAcademicLevels(true)
 println APITramites.instance.allAcademicLevels(false)
+
+println APITramites.instance.findPersonBy(0, "a.rescalvo")
