e586413b7f8ec61f9b69e25bc0852adf1faa53f7
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-15 08:33:32 -0600

    Agrego mensaje de Log!

--- rest_services.groovy
+++ rest_services.groovy
@@ -1,9 +1,11 @@
-@Grapes(
-  @Grab(group='com.github.groovy-wslite', module='groovy-wslite', version='1.1.3')
-)
+@Grapes([
+  @Grab(group='com.github.groovy-wslite', module='groovy-wslite', version='1.1.3'),
+  @Grab(group='log4j', module='log4j', version='1.2.17')
+])
 
 import wslite.rest.*
 import groovy.transform.Canonical
+import groovy.util.logging.Log4j
 
 @Canonical
 class AcademicLevel {
@@ -21,6 +23,7 @@ class EBCPerson {
 }
 
 @Singleton
+@Log4j
 class APITramites {
   def url = "https://api-tramites.ebc.edu.mx/api-tramites-escolares"
   def client = new RESTClient(url)
@@ -36,6 +39,8 @@ class APITramites {
     def response = client.get(
       path:'/v1/api/persons',
       query:[idEnrollment: idEnrollment, pidm: pidm, username: username])
+    if(response.statusCode == 204)
+      log.error("Usuario no encontrado!!!")
     new EBCPerson(response.json)
   }
 }
@@ -44,3 +49,4 @@ println APITramites.instance.allAcademicLevels(true)
 println APITramites.instance.allAcademicLevels(false)
 
 println APITramites.instance.findPersonBy(0, "a.rescalvo")
+println APITramites.instance.findPersonBy()
