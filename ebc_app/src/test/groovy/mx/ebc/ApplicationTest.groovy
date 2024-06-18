bdf29d405cc955581769b632849daad306b1dc01
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-18 10:08:37 -0600

    Agrego algunas pruebas de ejemplo con Java

--- ebc_app/src/test/groovy/mx/ebc/ApplicationTest.groovy
+++ ebc_app/src/test/groovy/mx/ebc/ApplicationTest.groovy
@@ -1,7 +1,11 @@
 package mx.ebc
 
-import org.junit.jupiter.api.Test
-import static org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.*
+import org.junit.jupiter.params.*
+import org.junit.jupiter.params.provider.*
+import java.util.stream.*
+import static org.junit.jupiter.api.Assertions.*
+import static org.junit.jupiter.api.DynamicTest.dynamicTest
 
 class ApplicationTest {
 
@@ -9,4 +13,30 @@ class ApplicationTest {
   void testSomething(){
     assertEquals(2, 1 + 1)
   }
+
+  @Test
+  void streamSum() {
+    assertTrue(Stream.of(1, 2, 3)
+      .mapToInt(i -> i)
+      .sum() > 5, () -> "Sum should be greater than 5")
+  }
+
+  @RepeatedTest(value=2, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+  void streamSumRepeated() {
+    assert Stream.of(1, 2, 3).mapToInt(i -> i).sum() == 6
+  }
+
+  private boolean isPalindrome(s) { s == s.reverse()  }
+
+  @ParameterizedTest
+  @ValueSource(strings = [ "racecar", "radar", "able was I ere I saw elba" ])
+  void palindromes(String candidate) {
+    assert isPalindrome(candidate)
+  }
+
+  @TestFactory
+  def dynamicTestCollection() {[
+  dynamicTest("Add test") { -> assert 1 + 1 == 2 },
+  dynamicTest("Multiply Test", () -> { assert 2 * 3 == 6 })
+  ]}
 }
