24ac1059173c7409bdb0be431edea0b2b43e7a5a
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-18 09:36:29 -0600

    Agrego el soporte de JUnit para Gradle

---
+++ ebc_app/src/test/groovy/mx/ebc/ApplicationTest.groovy
@@ -0,0 +1,12 @@
+package mx.ebc
+
+import org.junit.jupiter.api.Test
+import static org.junit.jupiter.api.Assertions.assertEquals
+
+class ApplicationTest {
+
+  @Test
+  void testSomething(){
+    assertEquals(2, 1 + 1)
+  }
+}
