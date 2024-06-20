ac05a7b9ebc6e97d461df05b933bff983e99e07b
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-20 11:47:42 -0600

    Agrego API para Trámites con Spec - ON

---
+++ ebc_app/src/main/groovy/mx/ebc/apitramiteson/APITramites.groovy
@@ -0,0 +1,9 @@
+package mx.ebc.apitramiteson
+
+@Singleton
+class APITramites {
+
+  List<AcademicLevel> allAcademicLevels(Boolean bannerLevel) {
+    []
+  }
+}
---
+++ ebc_app/src/test/groovy/mx/ebc/apitramiteson/APITramitesSpec.groovy
@@ -0,0 +1,18 @@
+package mx.ebc.apitramiteson
+
+import spock.lang.Specification
+
+class APITramitesSpec extends Specification {
+
+  def "Obtener todos los niveles académicos"(){
+    given:
+      APITramites instance = APITramites.instance
+    when:
+      List<AcademicLevel> levels = instance.allAcademicLevels(true)
+    then:
+      levels.size() > 0
+      levels.size()
+      levels
+
+  }
+}
