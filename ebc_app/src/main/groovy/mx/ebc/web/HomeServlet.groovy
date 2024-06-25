7d9eeb47ab0e2e1b22330dc3fad785fd50e4643d
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-25 11:39:09 -0600

    Agrego un doGet para Servlet

--- ebc_app/src/main/groovy/mx/ebc/web/HomeServlet.groovy
+++ ebc_app/src/main/groovy/mx/ebc/web/HomeServlet.groovy
@@ -1,8 +1,14 @@
 package mx.ebc.web
 
 import javax.servlet.annotation.WebServlet
-import javax.servlet.http.HttpServlet
+import javax.servlet.http.*
 
 @WebServlet("/")
 class HomeServlet extends HttpServlet {
+
+  void doGet(HttpServletRequest request, HttpServletResponse response){
+    println request.properties
+    println "*"*50
+    println response.properties
+  }
 }
