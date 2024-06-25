bf445348d01678e6b23b9bbef81071338297ee31
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-25 10:22:45 -0600

    Agrego el método findPersonBy

--- ebc_app/src/main/groovy/mx/ebc/apitramitesjj/APITramitesJJ.groovy
+++ ebc_app/src/main/groovy/mx/ebc/apitramitesjj/APITramitesJJ.groovy
@@ -12,9 +12,18 @@ class APITramitesJJ {
 
   List<AcademicLevel> allAcademicLevels(Boolean bannerLevel) {
     def response = client.get(
-      path: "/v1/api/academic_level", 
+      path: "/v1/api/academic_level",
       query: [bannerLevel: bannerLevel])
     log.debug "Response from URL: ${response}"
     response.json.collect { params -> new AcademicLevel(params) }
+  }
+
+  EBCPerson findPersonBy(Integer pidm = 0, String username = "", String idEnrollment = ""){
+    def response = client.get(
+      path:'/v1/api/persons',
+      query:[idEnrollment: idEnrollment, pidm: pidm, username: username])
+    if(response.statusCode == 204)
+      log.error("Usuario no encontrado!!!")
+    new EBCPerson(response.json)
   }
 }
