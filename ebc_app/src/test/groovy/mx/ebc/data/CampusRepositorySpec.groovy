46a2a5463e98253cfd27f97b74941661833dfd81
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-28 12:02:00 -0600

    Mando una lista de clases a la prueba

--- ebc_app/src/test/groovy/mx/ebc/data/CampusRepositorySpec.groovy
+++ ebc_app/src/test/groovy/mx/ebc/data/CampusRepositorySpec.groovy
@@ -1,18 +1,24 @@
 package mx.ebc.data
 
 import spock.lang.Specification
+import org.springframework.context.ApplicationContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.beans.factory.annotation.*
+import mx.ebc.config.DataConfig
 
-@ContextConfiguration(classes = DataConfig)
+@ContextConfiguration(classes = [DataConfig])
 class CampusRepositorySpec extends Specification {
 
   @Autowired
   CampusRepository campusRepository
 
+  @Autowired
+  ApplicationContext applicationContext
+
 
   void "should exist the bean in spring"() {
     expect:
+      applicationContext
       campusRepository
   }
 
