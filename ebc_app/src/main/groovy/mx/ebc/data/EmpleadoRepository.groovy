68941a8dc4f05ece3e6067f80cb571e78e7480a7
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-29 11:15:33 -0600

    Refactor: uso un mapper en la misma clase

--- ebc_app/src/main/groovy/mx/ebc/data/EmpleadoRepository.groovy
+++ ebc_app/src/main/groovy/mx/ebc/data/EmpleadoRepository.groovy
@@ -2,14 +2,24 @@ package mx.ebc.data
 
 import org.springframework.stereotype.*
 import org.springframework.beans.factory.annotation.*
-import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.*
 import mx.ebc.model.Empleado
+import java.sql.ResultSet
 
 @Repository
 class EmpleadoRepository {
 
   @Autowired
   JdbcTemplate jdbcTemplate
+
+  RowMapper empleadoMapper = { ResultSet rs, int rowNum ->
+    new Empleado(
+      trab_id: rs.getString("TRAB_ID"),
+      paterno: rs.getString("PATERNO"),
+      materno: rs.getString("MATERNO"),
+      nombre: rs.getString("NOMBRE")
+    )
+  } as RowMapper<Empleado>
 
   Integer countEmpleados(){
     jdbcTemplate.queryForObject('SELECT COUNT(*) as num FROM EMPLEADOSPRUEBA', Integer)
@@ -22,7 +32,8 @@ class EmpleadoRepository {
 
   List<Empleado> listEmpleados() {
     String query = "SELECT TRAB_ID, PATERNO, MATERNO, NOMBRE FROM EMPLEADOSPRUEBA"
-    jdbcTemplate.query(query, new EmpleadoRowMapper())
+    // jdbcTemplate.query(query, new EmpleadoRowMapper())
+    jdbcTemplate.query(query, empleadoMapper)
   }
 
 }
