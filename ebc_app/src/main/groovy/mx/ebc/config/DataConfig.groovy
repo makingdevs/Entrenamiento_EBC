4483ef6ed755119ef7b0cf31677080fcd16e90cb
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-06-29 09:49:14 -0600

    Defino un DataSource como Bean de Spring

--- ebc_app/src/main/groovy/mx/ebc/config/DataConfig.groovy
+++ ebc_app/src/main/groovy/mx/ebc/config/DataConfig.groovy
@@ -1,8 +1,20 @@
 package mx.ebc.config
 
 import org.springframework.context.annotation.*
+import org.springframework.jdbc.datasource.DriverManagerDataSource
 
 @Configuration
 @ComponentScan(basePackages = ["mx.ebc.data"])
 class DataConfig {
+
+  @Bean
+  DriverManagerDataSource dataSource(){
+    Map db = [
+      url:'jdbc:oracle:thin:@172.16.60.17:3521:EBCDEVL',
+      username:'baninst1',
+      password:'u_pick_it'
+    ]
+    new DriverManagerDataSource(db)
+  }
+
 }
