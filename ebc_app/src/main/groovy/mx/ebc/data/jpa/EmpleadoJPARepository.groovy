22223fcd2300825f736c4015bc60cba654400054
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-02 10:53:22 -0600

    Agrego JPA Query MethodS

--- ebc_app/src/main/groovy/mx/ebc/data/jpa/EmpleadoJPARepository.groovy
+++ ebc_app/src/main/groovy/mx/ebc/data/jpa/EmpleadoJPARepository.groovy
@@ -2,8 +2,8 @@ package mx.ebc.data.jpa
 
 import mx.ebc.model.Empleado
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
 
 interface EmpleadoJPARepository extends CrudRepository<Empleado, String> {
-
+    Optional<Empleado> findByNombre(String nombre)
+    Optional<Empleado> findByNombreAndPaterno(String nombre, String paterno)
 }
