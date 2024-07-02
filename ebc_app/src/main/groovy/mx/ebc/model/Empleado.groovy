7421f07a0c82181b8264b0ae4384806d29501a49
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-02 09:46:44 -0600

    Anoto Empleado con Persistencia

--- ebc_app/src/main/groovy/mx/ebc/model/Empleado.groovy
+++ ebc_app/src/main/groovy/mx/ebc/model/Empleado.groovy
@@ -1,9 +1,19 @@
 package mx.ebc.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+
 @groovy.transform.ToString
+@Entity
 class Empleado{
+  @Id
   String trab_id = new Date().time.toString()
+  @Column
   String paterno
+  @Column
   String materno
+  @Column
   String nombre
 }
