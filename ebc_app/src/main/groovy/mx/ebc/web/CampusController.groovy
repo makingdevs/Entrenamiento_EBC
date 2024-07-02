428c517c1a29817e4ce503ee3c3d643846c5f5cf
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-02 12:05:15 -0600

    Usamos un método cómo controller

--- ebc_app/src/main/groovy/mx/ebc/web/CampusController.groovy
+++ ebc_app/src/main/groovy/mx/ebc/web/CampusController.groovy
@@ -3,11 +3,19 @@ package mx.ebc.web
 import org.springframework.stereotype.*
 import org.springframework.beans.factory.annotation.*
 import mx.ebc.service.CampusService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class CampusController {
 
   @Autowired
   CampusService campusService
+
+  @ResponseBody
+  @RequestMapping("/hello")
+  String home() {
+    "Hola mundo!!! cómo estás?"
+  }
 
 }
