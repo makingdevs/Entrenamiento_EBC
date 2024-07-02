2994ae5d76fc7e1eedb14a6bdb81084ab71c89f1
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-02 12:04:58 -0600

    Habilitamos WebMVC

--- ebc_app/src/main/groovy/mx/ebc/config/WebConfig.groovy
+++ ebc_app/src/main/groovy/mx/ebc/config/WebConfig.groovy
@@ -1,8 +1,10 @@
 package mx.ebc.config
 
 import org.springframework.context.annotation.*
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = ["mx.ebc.web"])
 class WebConfig {
 }
