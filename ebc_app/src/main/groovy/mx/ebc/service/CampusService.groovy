bc4690a1eadf0fe312d8e277d7680fb9e1a62e60
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-02 13:23:47 -0600

    Complemento los métodos de la interfaz

--- ebc_app/src/main/groovy/mx/ebc/service/CampusService.groovy
+++ ebc_app/src/main/groovy/mx/ebc/service/CampusService.groovy
@@ -1,4 +1,9 @@
 package mx.ebc.service
 
+import mx.ebc.model.Empleado
+import mx.ebc.model.StvCamp
+
 interface CampusService {
+    List<StvCamp> allCampus()
+    Empleado assignCampusToPerson(Integer pidm, String stvcampCode)
 }
