2656fa211404a8d0e989457089ee1bb79d5eb4c2
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-04 10:20:25 -0600

    Defino métodos de operación

--- ebc_app/src/main/groovy/mx/ebc/web/EmpleadoController.groovy
+++ ebc_app/src/main/groovy/mx/ebc/web/EmpleadoController.groovy
@@ -7,22 +7,40 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.method.annotation.RequestParamMethodArgumentResolver
 
 @Controller
+@RequestMapping("/empleado")
 class EmpleadoController {
 
     @Autowired
     EmpleadoService empleadoService
 
-    @GetMapping("/empleado")
-    String showForm(Map model){
-        model.empleado = new Empleado()
+    @GetMapping
+    String showAll(Map model) {
+        List<Empleado> empleados = empleadoService.listAll()
+        model.empleados = empleados
+        "empleado/list"
+    }
+
+    @GetMapping("/show/:id")
+    String show(@RequestParam("id") String id, Map model){
+       Empleado empleado = empleadoService.findEmpleadoById(id)
+        model.empleado = empleado
         "empleado/show"
     }
 
-    @PostMapping("/empleado")
+    @GetMapping("/create")
+    String showForm(Map model){
+        model.empleado = new Empleado()
+        "empleado/form"
+    }
+
+    @PostMapping("/create")
     String submitForm(@ModelAttribute Empleado empleado){
         println empleado.properties
-        "empleado/show"
+        "empleado/form"
     }
 }
