37cd3bbbae8c5b08d7abc6eec9234025a74a87c0
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-04 13:04:00 -0600

    Definimos un BeanNameViewResolver para vistas en Excel y PDF

--- ebc_app/src/main/groovy/mx/ebc/config/WebConfig.groovy
+++ ebc_app/src/main/groovy/mx/ebc/config/WebConfig.groovy
@@ -14,6 +14,7 @@ class WebConfig implements WebMvcConfigurer {
 
     @Override
     void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.beanName()
         registry.jsp("/WEB-INF/views/", ".jsp")
     }
 
