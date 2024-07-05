ab53bf5c5cf314e232295f4d18ebd06f02f90126
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-05 13:04:33 -0600

    Compruebo beans de Spring

--- ebc_app_sb/src/test/groovy/mx/ebc/app/EbcAppSbApplicationTests.groovy
+++ ebc_app_sb/src/test/groovy/mx/ebc/app/EbcAppSbApplicationTests.groovy
@@ -1,13 +1,24 @@
 package mx.ebc.app
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+
+import javax.sql.DataSource
 
 @SpringBootTest
 class EbcAppSbApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext
+	@Autowired
+	DataSource dataSource
+
 	@Test
 	void contextLoads() {
+		assert applicationContext
+		assert !dataSource
 	}
 
 }
