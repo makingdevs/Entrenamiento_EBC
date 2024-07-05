ec0d195e4343e6b49a51c9fdd3701f5ef2a6b6ef
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-05 12:20:05 -0600

    Agrego nuevo proyecto de Spring Boot

---
+++ ebc_app_sb/src/main/groovy/mx/ebc/app/EbcAppSbApplication.groovy
@@ -0,0 +1,13 @@
+package mx.ebc.app
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EbcAppSbApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(EbcAppSbApplication, args)
+	}
+
+}
---
+++ ebc_app_sb/src/test/groovy/mx/ebc/app/EbcAppSbApplicationTests.groovy
@@ -0,0 +1,13 @@
+package mx.ebc.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EbcAppSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
