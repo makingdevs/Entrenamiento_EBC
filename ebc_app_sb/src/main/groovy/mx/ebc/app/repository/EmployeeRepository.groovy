724d9e223a650488d65710819b855f69c70b1126
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-09 12:55:02 -0600

    Agrego un Rest controller

--- ebc_app_sb/src/main/groovy/mx/ebc/app/repository/EmployeeRepository.groovy
+++ ebc_app_sb/src/main/groovy/mx/ebc/app/repository/EmployeeRepository.groovy
@@ -2,9 +2,13 @@ package mx.ebc.app.repository
 
 import mx.ebc.app.model.Employee
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.rest.webmvc.RepositoryRestController
 import org.springframework.stereotype.Repository
 
 @Repository
-interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+@RepositoryRestController(path = "/api/employees")
+interface EmployeeRepository extends PagingAndSortingRepository<Employee, Integer> {
 
 }
