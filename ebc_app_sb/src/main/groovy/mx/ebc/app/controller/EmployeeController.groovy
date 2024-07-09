283481d15ef171233ad4c6962dd878499e90fb28
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-09 12:21:40 -0600

    Agrego métodos para editar y borrar

--- ebc_app_sb/src/main/groovy/mx/ebc/app/controller/EmployeeController.groovy
+++ ebc_app_sb/src/main/groovy/mx/ebc/app/controller/EmployeeController.groovy
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -30,6 +31,22 @@ class EmployeeController {
   @PostMapping
   String saveEmployee(@ModelAttribute("employee") Employee employee){
     employeeRepository.save(employee)
+    "redirect:/employees"
+  }
+
+  @GetMapping("/edit/{id}")
+  String showEditForm(@PathVariable("id") Integer id, Map model){
+    Employee employee = employeeRepository.findById(id).orElseThrow({
+      new RuntimeException("No se encuentra el empleado")})
+    model.employee = employee
+    "employees/form"
+  }
+
+  @GetMapping("/delete/{id}")
+  String deleteEmployee(@PathVariable("id") Integer id, Map model){
+    Employee employee = employeeRepository.findById(id).orElseThrow({
+      new RuntimeException("No se encuentra el empleado")})
+    employeeRepository.delete(employee)
     "redirect:/employees"
   }
 }
