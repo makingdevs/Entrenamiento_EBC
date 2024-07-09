1b5279ba3ccbacfbfd1bd3509780f8b669488fdc
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-09 11:36:20 -0600

    Usamos una secuencia en la generación de ID

--- ebc_app_sb/src/main/groovy/mx/ebc/app/model/Employee.groovy
+++ ebc_app_sb/src/main/groovy/mx/ebc/app/model/Employee.groovy
@@ -5,6 +5,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.SequenceGenerator
 import javax.persistence.Table
 
 @Entity
@@ -12,11 +13,12 @@ import javax.persistence.Table
 class Employee {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "employee_jj_id_seq")
+  @SequenceGenerator(name="employee_jj_id_seq", sequenceName = "employee_jj_id_seq", allocationSize = 1)
   Integer id
-  @Column(name = "FIRST_NAME")
+  @Column(name = "firstname")
   String firstName
-  @Column(name = "LAST_NAME")
+  @Column(name = "lastname")
   String lastName
   String email
 
