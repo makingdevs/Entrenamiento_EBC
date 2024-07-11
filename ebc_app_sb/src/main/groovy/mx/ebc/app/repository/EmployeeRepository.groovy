ca9ec498f8d1435e50ec294b70c273e2d0085772
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-11 10:18:30 -0600

    Agrego un finder dinámico para usarse en REST o Web

--- ebc_app_sb/src/main/groovy/mx/ebc/app/repository/EmployeeRepository.groovy
+++ ebc_app_sb/src/main/groovy/mx/ebc/app/repository/EmployeeRepository.groovy
@@ -4,11 +4,12 @@ import mx.ebc.app.model.Employee
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.webmvc.RepositoryRestController
 import org.springframework.stereotype.Repository
 
 @Repository
 @RepositoryRestController(path = "/api/employees")
 interface EmployeeRepository extends PagingAndSortingRepository<Employee, Integer> {
-
+  List<Employee> findByLastName(@Param("lastname") String lastName)
 }
