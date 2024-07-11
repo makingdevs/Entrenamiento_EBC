f1ecaab790af32eae69e912c7bc76e91776db5df
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-11 10:17:48 -0600

    Cambio la URL de Web a /empleados

--- ebc_app_sb/src/main/groovy/mx/ebc/app/controller/EmployeeController.groovy
+++ ebc_app_sb/src/main/groovy/mx/ebc/app/controller/EmployeeController.groovy
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/employees")
+@RequestMapping("/empleados")
 class EmployeeController {
   @Autowired
   EmployeeRepository employeeRepository
