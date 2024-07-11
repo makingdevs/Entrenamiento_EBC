e95107002d87373f86e7dbdf62e67cb2ab5c1a5f
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-11 11:18:57 -0600

    Mapeo de Address

---
+++ ebc_app_sb/src/main/groovy/mx/ebc/app/model/Address.groovy
@@ -0,0 +1,24 @@
+package mx.ebc.app.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.persistence.SequenceGenerator
+import javax.persistence.Table
+
+@Entity
+@Table(name="ADDRESS_JJ")
+class Address {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_generator")
+  @SequenceGenerator(name="address_generator", sequenceName = "address_jj_id_seq", allocationSize = 1)
+  Long id
+  String street
+  String number
+  String zipCode
+
+  @ManyToOne
+  Employee employee
+}
