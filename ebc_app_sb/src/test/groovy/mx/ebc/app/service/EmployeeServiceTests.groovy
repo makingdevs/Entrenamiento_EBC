c9c05fc583979fa69bf3e34e517a69828cafe4af
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-12 09:33:25 -0600

    Poner orden de pruebas, y separar la creación con la actualización

--- ebc_app_sb/src/test/groovy/mx/ebc/app/service/EmployeeServiceTests.groovy
+++ ebc_app_sb/src/test/groovy/mx/ebc/app/service/EmployeeServiceTests.groovy
@@ -3,11 +3,15 @@ package mx.ebc.app.service
 import mx.ebc.app.model.Address
 import mx.ebc.app.model.Employee
 import mx.ebc.app.repository.EmployeeRepository
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation)
 class EmployeeServiceTests {
 
   @Autowired
@@ -16,16 +20,26 @@ class EmployeeServiceTests {
   @Autowired
   EmployeeRepository employeeRepository
 
+  static Integer employeeId
+
   @Test
-  void testAddAddressToEmployee(){
+  @Order(1)
+  void testCreateEmployee() {
     // Creo un Employee de manera inicial
     Employee employee = new Employee(firstName: "Juan", lastName: "Reyes", email: "juan@makingdevs.com")
     employee = employeeRepository.save(employee)
+    this.employeeId = employee.id
+    assert this.employeeId
+  }
 
+  @Test
+  @Order(2)
+  void testAddAddressToEmployee(){
+    assert this.employeeId
     // Creo un posible Address
-    Address address = new Address(street: "Some", number: "number", zipCode: "12345")
-    Employee updatedEmployee = employeeService.addAddressToEmployee(employee.id, address)
+    Address address = new Address(street: "Some", externalNumber: "number", zipCode: "12345")
+    Employee updatedEmployee = employeeService.addAddressToEmployee(this.employeeId, address)
 
-    println updatedEmployee.properties
+    assert updatedEmployee.addresses
   }
 }
