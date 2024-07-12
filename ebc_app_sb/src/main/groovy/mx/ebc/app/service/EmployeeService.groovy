d6926af1a2ffc893b8c6e3c09651d9e0a4fd4409
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-12 09:33:00 -0600

    USamos la búsqueda EAGER y manejamos el optional

--- ebc_app_sb/src/main/groovy/mx/ebc/app/service/EmployeeService.groovy
+++ ebc_app_sb/src/main/groovy/mx/ebc/app/service/EmployeeService.groovy
@@ -1,22 +1,21 @@
 package mx.ebc.app.service
 
+import groovy.transform.CompileStatic
 import mx.ebc.app.model.Address
 import mx.ebc.app.model.Employee
 import mx.ebc.app.repository.AddressRepository
 import mx.ebc.app.repository.EmployeeRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class EmployeeService {
   @Autowired
   EmployeeRepository employeeRepository
-  @Autowired
-  AddressRepository addressRepository
 
   Employee addAddressToEmployee(Integer employeeId, Address address){
-    Employee employee = employeeRepository.findById(employeeId).orElseThrow({
-      new RuntimeException("No se encuentra el empleado")})
+    Employee employee = employeeRepository.findByIdWithAddresses(employeeId)?.get()
     employee.addresses << address
     employeeRepository.save(employee)
   }
