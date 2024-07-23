7bd56659d29d89b95abb9a8d21bed1f7f0643b91
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-23 11:15:34 -0600

    Manejo el formulario para guardar el address

--- EBC_app_grails/grails-app/controllers/mx/ebc/EmployeeAddressController.groovy
+++ EBC_app_grails/grails-app/controllers/mx/ebc/EmployeeAddressController.groovy
@@ -3,11 +3,26 @@ package mx.ebc
 class EmployeeAddressController {
 
   EmployeeService employeeService
+  AddressService addressService
+
+  static allowedMethods = [saveAddress: "POST"]
 
   def index() {
     [
       message: "Hola mundo ${new Date()}",
       employees: employeeService.list([:])
     ]
+  }
+
+  def saveAddress(params) {
+    log.info "${params}"
+    Employee employee = employeeService.get(params.employeeId)
+    Address address = new Address(params)
+    address.employee = employee
+    address = addressService.save(address)
+
+    flash.message = message(code: 'default.created.message', args: [message(code: 'address.label', default: 'Address'), address.id])
+
+    redirect action: "index"
   }
 }
