f7085abf57c3b68353846a8bd100780918628298
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-23 09:54:29 -0600

    Inicializo contenido en la tabla Employee desde Bootstrap

--- EBC_app_grails/grails-app/init/ebc_app_grails/BootStrap.groovy
+++ EBC_app_grails/grails-app/init/ebc_app_grails/BootStrap.groovy
@@ -1,9 +1,31 @@
 package ebc_app_grails
 
+import mx.ebc.EmployeeService
+import mx.ebc.Employee
+
 class BootStrap {
 
-    def init = { servletContext ->
+  EmployeeService employeeService
+
+  def init = { servletContext ->
+    if(!employeeService.count()) {
+      String path = "/Users/makingdevs/Entrenamiento_EBC/EBC_app_grails/MOCK_DATA-2.csv"
+      String text = new File(path).text
+      text.split("\n").drop(1).collect() { line ->
+        def record = line.split(",")
+        Employee employee = new Employee(
+          id: record[0].toInteger(),
+          name: record[1],
+          lastName:  record[2],
+          bornDate: Date.parse("dd/mm/yyyy", record[3]),
+          weight: record[4].toInteger(),
+          active: record[5].toBoolean()
+        )
+        employeeService.save(employee)
+      }
     }
-    def destroy = {
-    }
+  }
+
+  def destroy = {
+  }
 }
