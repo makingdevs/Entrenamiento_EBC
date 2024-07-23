8469fcde48603ccadab09f62492abc404089c068
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-07-23 11:43:54 -0600

    Hago inyección de dependencias

--- EBC_app_grails/grails-app/conf/spring/resources.groovy
+++ EBC_app_grails/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,10 @@
 // Place your Spring DSL code here
 beans = {
+  jdbcTemplate(org.springframework.jdbc.core.JdbcTemplate) {
+    dataSource = ref('dataSource')
+  }
+
+  allTermsSp(mx.ebc.sp.AllTermsSP) {
+    jdbcTemplate = ref('jdbcTemplate')
+  }
 }
