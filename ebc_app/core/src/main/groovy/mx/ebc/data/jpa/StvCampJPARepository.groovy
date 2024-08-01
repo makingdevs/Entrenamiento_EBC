67706aca8a17ec79182226c36d9a75a0d38d6d26
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-08-01 12:45:19 -0600

    Ajusto pruebas

--- ebc_app/core/src/main/groovy/mx/ebc/data/jpa/StvCampJPARepository.groovy
+++ ebc_app/core/src/main/groovy/mx/ebc/data/jpa/StvCampJPARepository.groovy
@@ -2,7 +2,9 @@ package mx.ebc.data.jpa
 
 import mx.ebc.model.StvCamp
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.*
 
+@Repository
 interface StvCampJPARepository extends CrudRepository<StvCamp, String> {
 
 }
