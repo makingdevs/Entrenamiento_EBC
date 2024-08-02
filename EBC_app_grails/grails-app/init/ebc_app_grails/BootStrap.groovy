cca755b1ef1975c18a17e3c27cf93cb8dfd50e91
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-08-02 12:30:11 -0600

    Quito la carga de empleados desde bootsrap

--- EBC_app_grails/grails-app/init/ebc_app_grails/BootStrap.groovy
+++ EBC_app_grails/grails-app/init/ebc_app_grails/BootStrap.groovy
@@ -8,22 +8,22 @@ class BootStrap {
   EmployeeService employeeService
 
   def init = { servletContext ->
-    if(!employeeService.count()) {
-      String path = "/Users/makingdevs/Entrenamiento_EBC/EBC_app_grails/MOCK_DATA-2.csv"
-      String text = new File(path).text
-      text.split("\n").drop(1).collect() { line ->
-        def record = line.split(",")
-        Employee employee = new Employee(
-          id: record[0].toInteger(),
-          name: record[1],
-          lastName:  record[2],
-          bornDate: new Date().parse("dd/mm/yyyy", record[3]),
-          weight: record[4].toInteger(),
-          active: record[5].toBoolean()
-        )
-        employeeService.save(employee)
-      }
-    }
+    // if(!employeeService.count()) {
+    //   String path = "/Users/makingdevs/Entrenamiento_EBC/EBC_app_grails/MOCK_DATA-2.csv"
+    //   String text = new File(path).text
+    //   text.split("\n").drop(1).collect() { line ->
+    //     def record = line.split(",")
+    //     Employee employee = new Employee(
+    //       id: record[0].toInteger(),
+    //       name: record[1],
+    //       lastName:  record[2],
+    //       bornDate: new Date().parse("dd/mm/yyyy", record[3]),
+    //       weight: record[4].toInteger(),
+    //       active: record[5].toBoolean()
+    //     )
+    //     employeeService.save(employee)
+    //   }
+    // }
   }
 
   def destroy = {
