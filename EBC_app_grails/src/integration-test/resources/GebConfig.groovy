43174f4a6220aee1db52c2409b6a87b7fa0cdaf5
José Juan Reyes Zuñiga <juan@makingdevs.com>
2024-08-09 01:06:34 -0600

    Indent en GebConfig

--- EBC_app_grails/src/integration-test/resources/GebConfig.groovy
+++ EBC_app_grails/src/integration-test/resources/GebConfig.groovy
@@ -4,22 +4,22 @@ import org.openqa.selenium.firefox.FirefoxDriver
 
 environments {
 
-    // run via “./gradlew -Dgeb.env=chrome iT”
-    chrome {
-        driver = { new ChromeDriver() }
-    }
+  // run via “./gradlew -Dgeb.env=chrome iT”
+  chrome {
+    driver = { new ChromeDriver() }
+  }
 
-    // run via “./gradlew -Dgeb.env=chromeHeadless iT”
-    chromeHeadless {
-        driver = {
-            ChromeOptions o = new ChromeOptions()
-            o.addArguments('headless')
-            new ChromeDriver(o)
-        }
+  // run via “./gradlew -Dgeb.env=chromeHeadless iT”
+  chromeHeadless {
+    driver = {
+      ChromeOptions o = new ChromeOptions()
+      o.addArguments('headless')
+      new ChromeDriver(o)
     }
+  }
 
-    // run via “./gradlew -Dgeb.env=firefox iT”
-    firefox {
-        driver = { new FirefoxDriver() }
-    }
+  // run via “./gradlew -Dgeb.env=firefox iT”
+  firefox {
+    driver = { new FirefoxDriver() }
+  }
 }
